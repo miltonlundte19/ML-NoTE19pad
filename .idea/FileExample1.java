75f591920f5d1eec121d984a120cac2389facb3e
miltonlundte19 <milton.lund@elev.ga.ntig.se>
2021-02-12 11:20:55 +0100

    slut på lektion 2021-02-12

--- .idea/FileExample1.java
+++ .idea/FileExample1.java
@@ -3,7 +3,6 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-
 /**
  * This is a class
  * Created 2021-02-12
--- src/Notpadclone.java
+++ src/Notpadclone.java
@@ -1,2 +1,26 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Notpadclone {
+    public Notpadclone() {
+        clerbutton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textArea1.setText("");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Notpadclone");
+        frame.setContentPane(new Notpadclone().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private JTextArea textArea1;
+    private JPanel panel1;
+    private JButton clerbutton;
 }
